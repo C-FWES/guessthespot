c93a7dfb7dac69d8470fc37180453780ce04a7d6
C-FWES <353861933@educ.dpcdsb.org>
2024-03-06 17:18:15 -0500

    Display marker on map click

--- src/GuessModal.js
+++ src/GuessModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { GoogleMap } from "@react-google-maps/api";
+import { GoogleMap, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
     width: '360px',
@@ -13,10 +13,23 @@ const center = {
   };
 
 function GuessModal() {
+
+    const [markerPos, setMarkerPos] = useState(null) // store the position of a clicked marker
+
+    const handleMapClick = (e) => { // get coordinates of a clicked point on the map
+        const clickedLat = e.latLng.lat();
+        const clickedLng = e.latLng.lng();
+        setMarkerPos({lat: clickedLat, lng: clickedLng})
+    }
+
     return (
         <div className="guessModalContainer">
             <div className="guessModalMapContainer">
-                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}/>
+                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onClick={handleMapClick}>
+                {markerPos && (
+                    <Marker position={markerPos} />
+                )}
+                </GoogleMap>
             </div>
             <div className="guessModalActionContainer">
                 <button className="guessModalAction">
