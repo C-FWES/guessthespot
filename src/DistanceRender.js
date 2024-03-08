cfae2f19b3b88996fec611ec1f4325f7c2edc476
C-FWES <353861933@educ.dpcdsb.org>
2024-03-08 18:04:49 -0500

    Display polyline between guess and location

--- src/DistanceRender.js
+++ src/DistanceRender.js
@@ -2,33 +2,42 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Modal from 'react-modal'
 import { useLocation } from 'react-router-dom'
-import { GoogleMap, Marker } from "@react-google-maps/api";
+import { GoogleMap, Marker, Polyline } from "@react-google-maps/api";
 import './App.css'
 
 const containerStyle = {
-    width: '500px',
-    height: '500px',
+    width: '100%',
+    height: '100%',
 }
 
 function DistanceRender() {
 
     const { state } = useLocation();
     const [markers, setMarkers] = useState([]);
+    const [path, setPath] = useState([])
 
     useEffect(() => {
         if (state) {
             const { markerPos, mapCoordinates } = state;
             setMarkers([markerPos, mapCoordinates]);
+            setPath([markerPos, mapCoordinates]);
         }
     }, [state]);
 
     return (
-        <div>
+        <div className="distanceRender">
             {
             <GoogleMap 
             mapContainerStyle={containerStyle}
             center={markers[0]}
-            zoom={0}>
+            zoom={3}>
+                {path.length > 0 &&
+                <Polyline path={markers} options={{
+                    strokeColor: "#000000",
+                    strokeOpacity: 1,
+                    strokeWeight: 2
+                }}/> 
+                }
                 {markers.map((marker, index) => <Marker key={index} position={marker}/>)}
             </GoogleMap>
             }
