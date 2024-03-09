4818c010a169bb6707bd4f16e63f5af4c2e2c521
C-FWES <353861933@educ.dpcdsb.org>
2024-03-09 10:21:33 -0500

    Add 60 second timer to scorebar

---
+++ src/GameController.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GameController() {
+    return (
+        <div className="gameControllerContainer">
+            
+        </div>
+    )
+}
+
+export default GameController();
--- src/ScoreModal.js
+++ src/ScoreModal.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 function ScoreModal() {
 
     const [score, setScore] = useState(19999)
     const [round, setRound] = useState(1)
+    const [timer, setTimer] = useState(60) // 1 minute timer
+
+    useEffect(() => {
+        const countdown = setInterval(() => {
+            setTimer((prevTimer) => prevTimer - 1) // take 1 second increments
+        }, 1000);
+
+        return () => clearInterval(countdown);
+    }, [])
 
     return(
         <div className="scoreModalContainer">
             <div className="hero">GuessTheSpot by Wesley Ou</div>
             <div className="score">Score: {score}</div>
             <div className="round">Round: {round}/5</div>
+            <div className="timer">Time left: {timer} seconds</div>
         </div>
     )
 }
